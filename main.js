6cd17c73b40c61cbf06a7b9bbc5446d747cd0d7f
cptacm <daanc@localhost.localdomain>
2020-11-26 16:19:53 +0100

    make it better

---
+++ main.js
@@ -0,0 +1,44 @@
+// Map
+var mymap = L.map("mapid").setView([51.088, 5.22], 13);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3B0LWFjbSIsImEiOiJja2NtMjFxdzEyYjljMnRueHVuOXRpOXl0In0.mzdk1bXj7WiLEElTiEV96A",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: "your.mapbox.access.token",
+  }
+).addTo(mymap);
+
+// Change style of navbar on scroll
+window.onscroll = function () {
+  myFunction();
+};
+function myFunction() {
+  var navbar = document.getElementById("myNavbar");
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+  } else {
+    navbar.className = navbar.className.replace(
+      " w3-card w3-animate-top w3-white",
+      ""
+    );
+  }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
